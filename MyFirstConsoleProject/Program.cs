c1da1020bd35ed4edd8d9bcea9bd80122e368e02
Titus Moore <titusdmoore@gmail.com>
2019-07-23 16:24:33 -0400

    Add project files.

---
+++ MyFirstConsoleProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyFirstConsoleProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            int answer = Arithmetic.Add(3, 7);
+            Console.WriteLine("3 + 7 = " + answer);
+            answer = Arithmetic.Subtract(10, 6);
+            Console.WriteLine("10 - 6 = " + answer);
+            answer = Arithmetic.Mult(5, 7);
+            Console.WriteLine("5 * 7 = " + answer);
+            answer = Arithmetic.Div(7, 2);
+            Console.WriteLine("7 / 2 = " + answer);
+            answer = Arithmetic.Modulus(100, 7);
+            Console.WriteLine("100 % 7 = " + answer);
+        }
+    }
+    class Arithmetic {
+        public static int Add ( int a, int b) {
+            return a + b;
+        }
+        public static int Subtract ( int a, int b) {
+            return a - b;
+        }
+        public static int Mult ( int a, int b) {
+            return a * b;
+        }
+        public static int Div ( int a, int b ) {
+            if (b != 0) {
+                return a / b;
+            }else {
+                return b;
+            }
+        }
+        public static int Modulus ( int a, int b) {
+            return a % b;
+        }
+    }
+}
